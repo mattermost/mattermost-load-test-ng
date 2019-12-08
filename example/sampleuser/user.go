d47d8402bfc5c8b24c02e87ce19de4dd19a93b22
Eli Yukelzon <reflog@gmail.com>
2019-12-08 10:35:41 +0200

    added GetTeamsUnread

--- example/sampleuser/user.go
+++ example/sampleuser/user.go
@@ -205,3 +205,7 @@ func (u *SampleUser) CreateTeam(team *model.Team) (string, error) {
 func (u *SampleUser) AddTeamMember(teamId, userId string) error {
 	return nil
 }
+
+func (ue *SampleUser) GetTeamsUnread(teamIdToExclude string) ([]*model.TeamUnread, error) {
+	return []*model.TeamUnread{}, nil
+}
