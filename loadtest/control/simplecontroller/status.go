e9157596de1d37a47fe62919926a1b235d5dbbed
streamer45 <cstcld91@gmail.com>
2020-01-09 11:36:27 +0100

    Extract getErrOrigin function

--- loadtest/control/simplecontroller/status.go
+++ loadtest/control/simplecontroller/status.go
@@ -1,15 +1,8 @@
 package simplecontroller
 
 import (
-	"fmt"
-	"os"
-	"runtime"
-	"strings"
-
 	"github.com/mattermost/mattermost-load-test-ng/loadtest/control"
 )
-
-const pkgPath string = "github.com/mattermost/mattermost-load-test-ng/loadtest/control/"
 
 func (c *SimpleController) newInfoStatus(info string) control.UserStatus {
 	return control.UserStatus{
@@ -22,14 +15,7 @@ func (c *SimpleController) newInfoStatus(info string) control.UserStatus {
 }
 
 func (c *SimpleController) newErrorStatus(err error) control.UserStatus {
-	var origin string
-	if pc, file, line, ok := runtime.Caller(1); ok {
-		if f := runtime.FuncForPC(pc); f != nil {
-			if wd, err := os.Getwd(); err == nil {
-				origin = fmt.Sprintf("%s %s:%d", strings.TrimPrefix(f.Name(), pkgPath), strings.TrimPrefix(file, wd+"/"), line)
-			}
-		}
-	}
+	origin := getErrOrigin()
 	return control.UserStatus{
 		ControllerId: c.id,
 		User:         c.user,
---
+++ loadtest/control/simplecontroller/utils.go
@@ -0,0 +1,22 @@
+package simplecontroller
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+)
+
+const pkgPath = "github.com/mattermost/mattermost-load-test-ng/loadtest/control/"
+
+func getErrOrigin() string {
+	var origin string
+	if pc, file, line, ok := runtime.Caller(2); ok {
+		if f := runtime.FuncForPC(pc); f != nil {
+			if wd, err := os.Getwd(); err == nil {
+				origin = fmt.Sprintf("%s %s:%d", strings.TrimPrefix(f.Name(), pkgPath), strings.TrimPrefix(file, wd+string(os.PathSeparator)), line)
+			}
+		}
+	}
+	return origin
+}
