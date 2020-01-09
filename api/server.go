8ca57b63a77d5473025f585cd1e25f64f3ead4e9
Eli Yukelzon <reflog@gmail.com>
2020-01-09 16:56:14 +0200

    renames

--- api/server.go
+++ api/server.go
@@ -36,7 +36,7 @@ func writeJsonResponse(w http.ResponseWriter, data interface{}) {
 	_ = json.NewEncoder(w).Encode(data)
 }
 
-func (a *API) createLoadTestHandler(w http.ResponseWriter, r *http.Request) {
+func (a *API) createLoadAgentHandler(w http.ResponseWriter, r *http.Request) {
 	var config config.LoadTestConfig
 	err := json.NewDecoder(r.Body).Decode(&config)
 	if err != nil {
@@ -62,7 +62,7 @@ func (a *API) createLoadTestHandler(w http.ResponseWriter, r *http.Request) {
 	lt := loadtest.New(&config, newSimpleController)
 	a.agents[u.String()] = lt
 
-	writeJsonResponse(w, map[string]string{"loadTestId": u.String()})
+	writeJsonResponse(w, map[string]string{"loadAgentId": u.String()})
 }
 
 func (a *API) getLoadTestById(w http.ResponseWriter, r *http.Request) (*loadtest.LoadTester, error) {
@@ -70,14 +70,14 @@ func (a *API) getLoadTestById(w http.ResponseWriter, r *http.Request) (*loadtest
 	id := vars["id"]
 	lt, ok := a.agents[id]
 	if !ok {
-		err := fmt.Errorf("Load test with id %s not found", id)
+		err := fmt.Errorf("Load-test agent with id %s not found", id)
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return nil, err
 	}
 	return lt, nil
 }
 
-func (a *API) runLoadTestHandler(w http.ResponseWriter, r *http.Request) {
+func (a *API) runLoadAgentHandler(w http.ResponseWriter, r *http.Request) {
 	lt, err := a.getLoadTestById(w, r)
 	if err != nil {
 		return
@@ -86,10 +86,10 @@ func (a *API) runLoadTestHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	writeJsonResponse(w, map[string]interface{}{"message": "load test started", "status": MockStatus{}})
+	writeJsonResponse(w, map[string]interface{}{"message": "Load-test agent started", "status": MockStatus{}})
 }
 
-func (a *API) stopLoadTestHandler(w http.ResponseWriter, r *http.Request) {
+func (a *API) stopLoadAgentHandler(w http.ResponseWriter, r *http.Request) {
 	lt, err := a.getLoadTestById(w, r)
 	if err != nil {
 		return
@@ -98,7 +98,7 @@ func (a *API) stopLoadTestHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	writeJsonResponse(w, map[string]interface{}{"message": "load test stopped", "status": MockStatus{}})
+	writeJsonResponse(w, map[string]interface{}{"message": "Load-test agent stopped", "status": MockStatus{}})
 }
 
 func (a *API) destroyLoadTestHandler(w http.ResponseWriter, r *http.Request) {
@@ -110,10 +110,10 @@ func (a *API) destroyLoadTestHandler(w http.ResponseWriter, r *http.Request) {
 	_ = lt.Stop() // we are ignoring the error here in case the load test was previously stopped
 
 	delete(a.agents, mux.Vars(r)["id"])
-	writeJsonResponse(w, map[string]string{"message": "load test destroyed"})
+	writeJsonResponse(w, map[string]string{"message": "Load-test agent destroyed"})
 }
 
-func (a *API) getLoadTestStatusHandler(w http.ResponseWriter, r *http.Request) {
+func (a *API) getLoadAgentStatusHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := a.getLoadTestById(w, r)
 	if err != nil {
 		return
@@ -178,11 +178,11 @@ func SetupAPIRouter() *mux.Router {
 	r := router.PathPrefix("/loadagent").Subrouter()
 
 	agent := API{agents: make(map[string]*loadtest.LoadTester)}
-	r.HandleFunc("/create", agent.createLoadTestHandler).Methods("POST")
-	r.HandleFunc("/{id}/run", agent.runLoadTestHandler).Methods("POST")
-	r.HandleFunc("/{id}/stop", agent.stopLoadTestHandler).Methods("POST")
+	r.HandleFunc("/create", agent.createLoadAgentHandler).Methods("POST")
+	r.HandleFunc("/{id}/run", agent.runLoadAgentHandler).Methods("POST")
+	r.HandleFunc("/{id}/stop", agent.stopLoadAgentHandler).Methods("POST")
 	r.HandleFunc("/{id}", agent.destroyLoadTestHandler).Methods("DELETE")
-	r.HandleFunc("/{id}/status", agent.getLoadTestStatusHandler).Methods("GET")
+	r.HandleFunc("/{id}/status", agent.getLoadAgentStatusHandler).Methods("GET")
 	r.HandleFunc("/{id}/user/add", agent.addUserHandler).Methods("POST").Queries("amount", "{[0-9]*?}")
 	r.HandleFunc("/{id}/user/remove", agent.removeUserHandler).Methods("POST").Queries("amount", "{[0-9]*?}")
 	return router
