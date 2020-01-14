e45cbb4306251492ec99e7743db0bb0808154b77
Esdras Beleza de Noronha <github@contato.esdrasbeleza.com>
2020-01-14 23:31:52 +0000

    Improve logic

--- cmd/metricswatcher/prometheushelper/prometheushelper.go
+++ cmd/metricswatcher/prometheushelper/prometheushelper.go
@@ -51,20 +51,17 @@ func (p PrometheusHelper) VectorFirst(query string) (float64, error) {
 }
 
 func (p PrometheusHelper) extractNumericValueFromFirstElement(value model.Value) (float64, error) {
-	textValue := ""
-
-	if value.Type() == model.ValVector {
-		vec, _ := value.(model.Vector)
-
-		if len(vec) == 0 {
-			return 0, errors.New("Vector has length = 0")
-		}
-
-		textValue = vec[0].Value.String()
-	} else {
+	if value.Type() != model.ValVector {
 		return 0, fmt.Errorf("Expected a vector, got a %s", value.Type().String())
 	}
 
+	vec, _ := value.(model.Vector)
+
+	if len(vec) == 0 {
+		return 0, errors.New("Vector has length = 0")
+	}
+
+	textValue := vec[0].Value.String()
 	numericValue, err := strconv.ParseFloat(textValue, 64)
 
 	if err != nil {
