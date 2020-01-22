54a96835e6c4c5de549e2ccabbc78323adb59641
Agniva De Sarker <agnivade@yahoo.co.in>
2020-01-22 14:10:27 +0530

    MM-21900: fix panic in userentity.postsMapToSlice (#106)
    If we are setting 0 len with x cap, we should use append call.
    If we are setting x len, we should use indices.
    
    We were mixing the two. Fixed it now to set x len.

--- loadtest/user/userentity/utils.go
+++ loadtest/user/userentity/utils.go
@@ -3,7 +3,7 @@ package userentity
 import "github.com/mattermost/mattermost-server/v5/model"
 
 func postsMapToSlice(postsMap map[string]*model.Post) []*model.Post {
-	posts := make([]*model.Post, 0, len(postsMap))
+	posts := make([]*model.Post, len(postsMap))
 	i := 0
 	for _, v := range postsMap {
 		posts[i] = v
