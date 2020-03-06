edad315ef5ce3f76b0c7be451112e5bf405decaa
Agniva De Sarker <agnivade@yahoo.co.in>
2020-03-06 08:54:49 +0530

    MM-22976: Refactor ssh commands to a separate package (#130)
    * Refactor ssh commands to a separate package
    
    - Created a new package ssh with new methods
    RunCommand and Upload.
    - Fix a bug with the DB cluster name check where
    it was checking for "-" instead of checking for a letter.
    - Fix a bug where variables were not passed during destroy
    now that default values are not there anymore.
    
    * Incorporated review comments

---
+++ terraform/ssh/ssh.go
@@ -0,0 +1,79 @@
+// Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package ssh
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+// Client is a wrapper type over a ssh connection
+// that takes care of creating a channel and running
+// commands in a single method.
+type Client struct {
+	client *ssh.Client
+}
+
+// NewClient returns a Client object by dialing
+// to the local ssh agent.
+func NewClient(ip string) (*Client, error) {
+	conn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	if err != nil {
+		return nil, err
+	}
+
+	config := &ssh.ClientConfig{
+		User: "ubuntu",
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeysCallback(agent.NewClient(conn).Signers),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	sshc, err := ssh.Dial("tcp", ip+":22", config)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{client: sshc}, nil
+}
+
+// RunCommand runs a given command in a new ssh session.
+func (sshc *Client) RunCommand(cmd string) error {
+	sess, err := sshc.client.NewSession()
+	if err != nil {
+		return err
+	}
+	defer sess.Close()
+
+	return sess.Run(cmd)
+}
+
+// Upload uploads a given src object to a given destination file.
+func (sshc *Client) Upload(src io.Reader, dst string) error {
+	if strings.ContainsAny(dst, `'\`) {
+		// TODO: copied from load-test repo. Need to be improved
+		// by using an actual sftp library.
+		return fmt.Errorf("shell quoting not actually implemented. don't use weird paths")
+	}
+
+	sess, err := sshc.client.NewSession()
+	if err != nil {
+		return err
+	}
+	defer sess.Close()
+
+	sess.Stdin = src
+	return sess.Run("cat > " + "'" + dst + "'")
+}
+
+// Close closes the underlying connection.
+func (sshc *Client) Close() error {
+	return sshc.client.Close()
+}
