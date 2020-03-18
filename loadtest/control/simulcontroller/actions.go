a07f923e84730bf3021232403cead9fd0f94162e
Claudio Costa <cstcld91@gmail.com>
2020-03-18 19:26:33 +0100

    Implement switchTeam and switchChannel for SimulController

--- loadtest/control/simulcontroller/actions.go
+++ loadtest/control/simulcontroller/actions.go
@@ -4,7 +4,13 @@
 package simulcontroller
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/mattermost/mattermost-load-test-ng/loadtest/control"
+	"github.com/mattermost/mattermost-load-test-ng/loadtest/user"
+
+	"github.com/mattermost/mattermost-server/v5/model"
 )
 
 type userAction struct {
@@ -32,4 +38,109 @@ func (c *SimulController) reload(full bool) control.UserActionResponse {
 	}
 
 	return control.Reload(c.user)
+}
+
+func (c *SimulController) joinTeam(u user.User) control.UserActionResponse {
+	userStore := u.Store()
+	userId := userStore.Id()
+	teamIds, err := u.GetAllTeams(0, 100)
+	if err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+	for _, teamId := range teamIds {
+		tm, err := userStore.TeamMember(teamId, userId)
+		if err != nil {
+			return control.UserActionResponse{Err: control.NewUserError(err)}
+		}
+		if tm.UserId == "" {
+			if err := u.AddTeamMember(teamId, userId); err != nil {
+				return control.UserActionResponse{Err: control.NewUserError(err)}
+			}
+			c.status <- c.newInfoStatus(fmt.Sprintf("joined team %s", teamId))
+			break
+		}
+	}
+	return c.switchTeam(u)
+}
+
+func (c *SimulController) switchTeam(u user.User) control.UserActionResponse {
+	// TODO: change this to make sure we don't pick the same team we are
+	// currently in.
+	team, err := u.Store().RandomTeamJoined()
+	if err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	if err := u.SetCurrentTeam(&team); err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	if _, err := u.GetChannelsForTeamForUser(team.Id, u.Store().Id()); err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	if err := u.GetChannelMembersForUser(u.Store().Id(), team.Id); err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	if _, err := u.GetTeamsUnread(""); err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	// TODO: use more realistic data.
+	var userIds []string
+	userIds = append(userIds, u.Store().Id())
+	if err := u.GetUsersStatusesByIds(userIds); err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	c.status <- c.newInfoStatus(fmt.Sprintf("switched to team %s", team.Id))
+
+	// We should probably keep track of the last channel viewed in the team but
+	// for now we can simplify and randomly pick one each time.
+
+	return switchChannel(u)
+}
+
+func switchChannel(u user.User) control.UserActionResponse {
+	team, err := u.Store().CurrentTeam()
+	if err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	} else if team == nil {
+		return control.UserActionResponse{Err: control.NewUserError(fmt.Errorf("current team should be set"))}
+	}
+
+	// TODO: change this to make sure we don't pick the same channel we are
+	// currently in.
+	channel, err := u.Store().RandomChannelJoined(team.Id)
+	if err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	if current, err := u.Store().CurrentChannel(); err != nil {
+	} else if current != nil {
+		// Somehow the webapp does a view to the current channel before switching.
+		if _, err := u.ViewChannel(&model.ChannelView{ChannelId: current.Id}); err != nil {
+			return control.UserActionResponse{Err: control.NewUserError(err)}
+		}
+	}
+
+	if err := u.SetCurrentChannel(&channel); err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	// TODO: use the information returned here to figure out how to properly fetch posts.
+	if _, err := u.ViewChannel(&model.ChannelView{ChannelId: channel.Id}); err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	if err := u.GetChannelStats(channel.Id); err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	if err := u.GetPostsSince(channel.Id, time.Now().Add(-1*time.Minute).Unix()*1000); err != nil {
+		return control.UserActionResponse{Err: control.NewUserError(err)}
+	}
+
+	return control.UserActionResponse{Info: fmt.Sprintf("switched to channel %s", channel.Id)}
 }
