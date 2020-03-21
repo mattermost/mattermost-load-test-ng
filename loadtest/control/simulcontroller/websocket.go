b6b1ac52a167ba9f10c051b77b58c4d0d9824fc4
Claudio Costa <cstcld91@gmail.com>
2020-03-21 11:07:35 +0100

    Handle user_typing event in SimulController ws handler

--- loadtest/control/simulcontroller/websocket.go
+++ loadtest/control/simulcontroller/websocket.go
@@ -4,7 +4,9 @@
 package simulcontroller
 
 import (
-// "github.com/mattermost/mattermost-server/v5/model"
+	"fmt"
+
+	"github.com/mattermost/mattermost-server/v5/model"
 )
 
 // wsEventHandler listens for WebSocket events to be handled.
@@ -14,7 +16,48 @@ import (
 func (c *SimulController) wsEventHandler() {
 	for ev := range c.user.Events() {
 		switch ev.EventType() {
-		default:
+		case model.WEBSOCKET_EVENT_TYPING:
+			userId, ok := ev.Data["user_id"].(string)
+			if !ok || len(userId) == 0 {
+				c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: invalid data found in event data"))
+				break
+			}
+
+			user, err := c.user.Store().GetUser(userId)
+			if err != nil {
+				c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUser failed %w", err))
+				break
+			}
+
+			// The user was found, we check if we have the status for it.
+			if len(user.Id) != 0 {
+				status, err := c.user.Store().Status(userId)
+				if err != nil {
+					c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: Status failed %w", err))
+					break
+				}
+
+				// If we can't find the user status in the store we fetch it.
+				if len(status.UserId) == 0 {
+					if err := c.user.GetUsersStatusesByIds([]string{user.Id}); err != nil {
+						c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUsersStatusesByIds failed %w", err))
+						break
+					}
+				}
+
+				break
+			}
+
+			// We couldn't find the user so we fetch it and its status.
+			if _, err := c.user.GetUsersByIds([]string{userId}); err != nil {
+				c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUsersByIds failed %w", err))
+				break
+			}
+
+			if err := c.user.GetUsersStatusesByIds([]string{userId}); err != nil {
+				c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUsersStatusesByIds failed %w", err))
+				break
+			}
 		}
 	}
 }
