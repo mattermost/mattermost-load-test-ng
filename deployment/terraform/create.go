aa44086aedbee62e358ed08b80a64c3a3916eaca
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2020-04-03 22:23:55 +0300

    deployment/terraform: fix a bug fith the string interpretation

--- deployment/terraform/create.go
+++ deployment/terraform/create.go
@@ -411,7 +411,7 @@ func (t *Terraform) displayInfo(output *terraformOutput) {
 	if len(output.Agents.Value) > 0 {
 		mlog.Info("Coordinator:" + output.Agents.Value[0].PublicIP)
 		runcmd := "go run ./cmd/ltctl"
-		if strings.HasSuffix(os.Args[0], "ltctl") {
+		if strings.HasPrefix(os.Args[0], "ltctl") {
 			runcmd = "ltctl"
 		}
 		mlog.Info(fmt.Sprintf("To start coordinator, you can use %q command.", runcmd+" loadtest start"))
