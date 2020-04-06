3c5b543a00646e4a290470bead3dd86f360dc18c
Claudio Costa <cstcld91@gmail.com>
2020-04-06 18:20:30 +0200

    Add doc for interface

--- loadtest/control/config.go
+++ loadtest/control/config.go
@@ -3,6 +3,8 @@
 
 package control
 
+// Config is an abstraction over Config structures provided by
+// UserController implementations.
 type Config interface {
 	// IsValid reports whether the Config is valid or not.
 	// Returns an error if the validation fails.
