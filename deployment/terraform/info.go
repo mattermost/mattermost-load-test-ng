524b79fe90837d89110a0f37ce1f56726380d99e
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2020-04-20 18:04:39 +0300

    deployment/terraform: print app server ips

--- deployment/terraform/info.go
+++ deployment/terraform/info.go
@@ -24,15 +24,15 @@ func (t *Terraform) displayInfo(output *terraformOutput) {
 	fmt.Println("Deployment information:")
 	if len(output.Proxy.Value) > 0 {
 		fmt.Println("Mattermost URL: http://" + output.Proxy.Value[0].PublicDNS)
-		fmt.Println("App Servers:")
-		for _, instance := range output.Instances.Value {
-			fmt.Println("- " + instance.Tags.Name + ": " + instance.PublicIP)
-		}
 	} else {
 		fmt.Println("Mattermost URL: http://" + output.Instances.Value[0].PublicDNS + ":8065")
 	}
+	fmt.Println("App Server(s):")
+	for _, instance := range output.Instances.Value {
+		fmt.Println("- " + instance.Tags.Name + ": " + instance.PublicIP)
+	}
 
-	fmt.Println("Load Agents:")
+	fmt.Println("Load Agent(s):")
 	for _, agent := range output.Agents.Value {
 		fmt.Println("- " + agent.Tags.Name + ": " + agent.PublicIP)
 	}
