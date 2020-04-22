2d8e3384e8359501d17fe1292b46fa8e363be1f7
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2020-04-22 11:24:25 +0300

    loadtest/simulcontroller: make ws event calls non-blocking

--- loadtest/control/simulcontroller/websocket.go
+++ loadtest/control/simulcontroller/websocket.go
@@ -10,7 +10,7 @@ import (
 )
 
 // wsEventHandler listens for WebSocket events to be handled.
-// This is used to model user behaviour by responding to certain events with
+// This is used to model user behavior by responding to certain events with
 // the appropriate actions. It differs from userentity.wsEventHandler which is
 // instead used to manage the internal user state.
 func (c *SimulController) wsEventHandler() {
@@ -18,46 +18,50 @@ func (c *SimulController) wsEventHandler() {
 		switch ev.EventType() {
 		case model.WEBSOCKET_EVENT_TYPING:
 			userId, ok := ev.Data["user_id"].(string)
-			if !ok || len(userId) == 0 {
+			if !ok || userId == "" {
 				c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: invalid data found in event data"))
 				break
 			}
 
-			user, err := c.user.Store().GetUser(userId)
-			if err != nil {
-				c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUser failed %w", err))
-				break
-			}
+			go c.typingEventHandler(userId)
+		}
+	}
+}
 
-			// The user was found, we check if we have the status for it.
-			if len(user.Id) != 0 {
-				status, err := c.user.Store().Status(userId)
-				if err != nil {
-					c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: Status failed %w", err))
-					break
-				}
+func (c *SimulController) typingEventHandler(userId string) {
+	user, err := c.user.Store().GetUser(userId)
+	if err != nil {
+		c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUser failed %w", err))
+		return
+	}
 
-				// If we can't find the user status in the store we fetch it.
-				if len(status.UserId) == 0 {
-					if err := c.user.GetUsersStatusesByIds([]string{user.Id}); err != nil {
-						c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUsersStatusesByIds failed %w", err))
-						break
-					}
-				}
+	// The user was found, we check if we have the status for it.
+	if user.Id != "" {
+		status, err := c.user.Store().Status(userId)
+		if err != nil {
+			c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: Status failed %w", err))
+			return
+		}
 
-				break
-			}
-
-			// We couldn't find the user so we fetch it and its status.
-			if _, err := c.user.GetUsersByIds([]string{userId}); err != nil {
-				c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUsersByIds failed %w", err))
-				break
-			}
-
-			if err := c.user.GetUsersStatusesByIds([]string{userId}); err != nil {
+		// If we can't find the user status in the store we fetch it.
+		if status.UserId == "" {
+			if err := c.user.GetUsersStatusesByIds([]string{user.Id}); err != nil {
 				c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUsersStatusesByIds failed %w", err))
-				break
+				return
 			}
 		}
+
+		return
+	}
+
+	// We couldn't find the user so we fetch it and its status.
+	if _, err := c.user.GetUsersByIds([]string{userId}); err != nil {
+		c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUsersByIds failed %w", err))
+		return
+	}
+
+	if err := c.user.GetUsersStatusesByIds([]string{userId}); err != nil {
+		c.status <- c.newErrorStatus(fmt.Errorf("simulcontroller: GetUsersStatusesByIds failed %w", err))
+		return
 	}
 }
