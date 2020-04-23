299d95511e704b5e1bdb0d73fd9678caf700041f
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2020-04-23 13:56:48 +0300

    Update loadtest/control/simulcontroller/websocket.go
    Co-Authored-By: Claudio Costa <cstcld91@gmail.com>

--- loadtest/control/simulcontroller/websocket.go
+++ loadtest/control/simulcontroller/websocket.go
@@ -10,7 +10,7 @@ import (
 )
 
 // wsEventHandler listens for WebSocket events to be handled.
-// This is used to model user behavior by responding to certain events with
+// This is used to model user behaviour by responding to certain events with
 // the appropriate actions. It differs from userentity.wsEventHandler which is
 // instead used to manage the internal user state.
 func (c *SimulController) wsEventHandler(semaphore chan struct{}) {
