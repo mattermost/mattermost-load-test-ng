6a319f6ca4e3a359f932cc1669d822df3d04e30e
Agniva De Sarker <agnivade@yahoo.co.in>
2020-05-07 15:41:31 +0530

    Add comments

--- websocket/client_test.go
+++ websocket/client_test.go
@@ -56,77 +56,77 @@ func TestClose(t *testing.T) {
 
 	t.Run("Sudden", func(t *testing.T) {
 		url := strings.Replace(s.URL, "http://", "ws://", 1)
-		cli, err := NewClient4(url, "authToken")
+		c, err := NewClient4(url, "authToken")
 		require.Nil(t, err)
 
 		go func() {
 			// Just drain the event channel
-			for range cli.EventChannel {
+			for range c.EventChannel {
 			}
 		}()
 
-		err = cli.UserTyping("channelId", "parentId")
+		err = c.UserTyping("channelId", "parentId")
 		assert.Nil(t, err)
 
-		err = cli.conn.Close()
+		err = c.conn.Close()
 		assert.Nil(t, err)
 
 		// wait for a while for reader to exit
 		time.Sleep(200 * time.Millisecond)
 
 		// Verify that event channel is closed.
-		checkEventChan(cli.EventChannel)
+		checkEventChan(c.EventChannel)
 	})
 
 	t.Run("Normal", func(t *testing.T) {
 		url := strings.Replace(s.URL, "http://", "ws://", 1)
-		cli, err := NewClient4(url, "authToken")
+		c, err := NewClient4(url, "authToken")
 		require.Nil(t, err)
 
 		go func() {
 			// Just drain the event channel
-			for range cli.EventChannel {
+			for range c.EventChannel {
 			}
 		}()
 
-		err = cli.UserTyping("channelId", "parentId")
+		err = c.UserTyping("channelId", "parentId")
 		assert.Nil(t, err)
 
-		cli.Close()
+		c.Close()
 
 		// Verify that event channel is closed.
-		checkEventChan(cli.EventChannel)
+		checkEventChan(c.EventChannel)
 	})
 
 	t.Run("Concurrent", func(t *testing.T) {
 		url := strings.Replace(s.URL, "http://", "ws://", 1)
-		cli, err := NewClient4(url, "authToken")
+		c, err := NewClient4(url, "authToken")
 		require.Nil(t, err)
 
 		go func() {
 			// Just drain the event channel
-			for range cli.EventChannel {
+			for range c.EventChannel {
 			}
 		}()
 
-		err = cli.UserTyping("channelId", "parentId")
+		err = c.UserTyping("channelId", "parentId")
 		assert.Nil(t, err)
 
 		var wg sync.WaitGroup
 		wg.Add(2)
 		go func() {
 			defer wg.Done()
-			cli.Close()
+			c.Close()
 		}()
 
 		go func() {
 			defer wg.Done()
-			cli.conn.Close()
+			c.conn.Close()
 		}()
 
 		wg.Wait()
 		// Verify that event channel is closed.
-		checkEventChan(cli.EventChannel)
+		checkEventChan(c.EventChannel)
 	})
 }
 
@@ -138,85 +138,85 @@ func TestSendMessage(t *testing.T) {
 
 	t.Run("SendAfterSuddenClose", func(t *testing.T) {
 		url := strings.Replace(s.URL, "http://", "ws://", 1)
-		cli, err := NewClient4(url, "authToken")
+		c, err := NewClient4(url, "authToken")
 		require.Nil(t, err)
 
 		go func() {
 			// Just drain the event channel
-			for range cli.EventChannel {
+			for range c.EventChannel {
 			}
 		}()
 
-		err = cli.UserTyping("channelId", "parentId")
+		err = c.UserTyping("channelId", "parentId")
 		assert.Nil(t, err)
 
-		err = cli.conn.Close()
+		err = c.conn.Close()
 		assert.Nil(t, err)
 
-		err = cli.UserTyping("channelId2", "parentId2")
+		err = c.UserTyping("channelId2", "parentId2")
 		assert.NotNil(t, err)
 	})
 
 	t.Run("SendAfterClose", func(t *testing.T) {
 		url := strings.Replace(s.URL, "http://", "ws://", 1)
-		cli, err := NewClient4(url, "authToken")
+		c, err := NewClient4(url, "authToken")
 		require.Nil(t, err)
 
 		go func() {
 			// Just drain the event channel
-			for range cli.EventChannel {
+			for range c.EventChannel {
 			}
 		}()
 
-		err = cli.UserTyping("channelId", "parentId")
+		err = c.UserTyping("channelId", "parentId")
 		assert.Nil(t, err)
 
-		cli.Close()
+		c.Close()
 
-		err = cli.UserTyping("channelId2", "parentId2")
+		err = c.UserTyping("channelId2", "parentId2")
 		assert.NotNil(t, err)
 	})
 
 	t.Run("SendDuringSuddenClose", func(t *testing.T) {
 		url := strings.Replace(s.URL, "http://", "ws://", 1)
-		cli, err := NewClient4(url, "authToken")
+		c, err := NewClient4(url, "authToken")
 		require.Nil(t, err)
 
 		go func() {
 			// Just drain the event channel
-			for range cli.EventChannel {
+			for range c.EventChannel {
 			}
 		}()
 
-		err = cli.UserTyping("channelId", "parentId")
+		err = c.UserTyping("channelId", "parentId")
 		assert.Nil(t, err)
 
 		go func() {
-			cli.UserTyping("channelId2", "parentId2")
+			c.UserTyping("channelId2", "parentId2")
 		}()
 
-		err = cli.conn.Close()
+		err = c.conn.Close()
 		assert.Nil(t, err)
 	})
 
 	t.Run("SendDuringClose", func(t *testing.T) {
 		url := strings.Replace(s.URL, "http://", "ws://", 1)
-		cli, err := NewClient4(url, "authToken")
+		c, err := NewClient4(url, "authToken")
 		require.Nil(t, err)
 
 		go func() {
 			// Just drain the event channel
-			for range cli.EventChannel {
+			for range c.EventChannel {
 			}
 		}()
 
-		err = cli.UserTyping("channelId", "parentId")
+		err = c.UserTyping("channelId", "parentId")
 		assert.Nil(t, err)
 
 		go func() {
-			cli.UserTyping("channelId2", "parentId2")
+			c.UserTyping("channelId2", "parentId2")
 		}()
 
-		cli.Close()
+		c.Close()
 	})
 }
