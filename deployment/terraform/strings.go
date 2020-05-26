a69f68c1374a93ed2f0adcd492dd72033c1f9d70
Claudio Costa <cstcld91@gmail.com>
2020-05-26 15:12:04 +0200

    Make Prometheus targets human friendly

--- deployment/terraform/strings.go
+++ deployment/terraform/strings.go
@@ -24,22 +24,36 @@ WantedBy=multi-user.target
 
 const prometheusConfig = `
 global:
-  scrape_interval:     15s # By default, scrape targets every 15 seconds.
-  evaluation_interval: 15s # By default, scrape targets every 15 seconds.
+  scrape_interval:     5s
+  evaluation_interval: 5s
 
 # A scrape configuration containing exactly one endpoint to scrape:
 # Here it's Prometheus itself.
 scrape_configs:
-  - job_name: 'prometheus'
-    scrape_interval: 5s
-    scrape_timeout: 5s
-
+  - job_name: prometheus
     static_configs:
-        - targets: ['localhost:9090', %s]
-
+        - targets: ['metrics:9090']
   - job_name: node
     static_configs:
-        - targets: ['localhost:9100', %s]
+        - targets: ['metrics:9100',%s]
+  - job_name: mattermost
+    static_configs:
+        - targets: [%s]
+`
+
+const metricsHosts = `
+127.0.0.1 localhost
+
+# The following lines are desirable for IPv6 capable hosts
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+
+127.0.0.1 metrics
+%s
 `
 
 const nginxConfig = `
