0dc325d74df3086b2a4432e0a2f95098b18042ce
Jesse Hallam <jesse.hallam@gmail.com>
2020-11-17 21:18:28 -0400

    fix stop conditions for noopcontroller

--- loadtest/control/noopcontroller/websocket.go
+++ loadtest/control/noopcontroller/websocket.go
@@ -3,23 +3,12 @@
 
 package noopcontroller
 
-import (
-	"github.com/mattermost/mattermost-server/v5/model"
-)
-
 // wsEventHandler listens for WebSocket events to be handled.
 // This is used to model user behaviour by responding to certain events with
 // the appropriate actions. It differs from userentity.wsEventHandler which is
 // instead used to manage the internal user state.
 func (c *NoopController) wsEventHandler() {
-	for ev := range c.user.Events() {
-		switch ev.EventType() {
-		case model.WEBSOCKET_EVENT_USER_UPDATED:
-			// probably do something interesting ?
-		case model.WEBSOCKET_EVENT_STATUS_CHANGE:
-			// probably do something interesting ?
-		default:
-			// add other handlers as necessary.
-		}
+	for range c.user.Events() {
+		// do nothing
 	}
 }
