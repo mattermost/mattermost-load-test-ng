0609b94aadb52905f3c7cdc4fbdd5ed6c89a8a85
Claudio Costa <cstcld91@gmail.com>
2020-12-02 17:26:52 +0100

    Implement loginOrSignUp action

--- loadtest/control/simulcontroller/actions.go
+++ loadtest/control/simulcontroller/actions.go
@@ -106,6 +106,18 @@ func (c *SimulController) fullReload(u user.User) control.UserActionResponse {
 	return c.reload(true)
 }
 
+func (c *SimulController) loginOrSignUp(u user.User) control.UserActionResponse {
+	resp := c.login(u)
+	if resp.Err != nil {
+		if resp = control.SignUp(u); resp.Err != nil {
+			return resp
+		}
+		c.status <- c.newInfoStatus(resp.Info)
+		return c.login(u)
+	}
+	return resp
+}
+
 func (c *SimulController) login(u user.User) control.UserActionResponse {
 	for {
 		resp := control.Login(u)
@@ -115,6 +127,11 @@ func (c *SimulController) login(u user.User) control.UserActionResponse {
 				return resp
 			}
 			c.status <- c.newErrorStatus(err)
+		}
+
+		errId := resp.Err.(*control.UserError).Err.(*model.AppError).Id
+		if errId == "api.user.login.invalid_credentials_email_username" {
+			return resp
 		}
 
 		c.status <- c.newErrorStatus(resp.Err)
