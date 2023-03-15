a363a5b2cefde37db3f63430677c62e1d9fe1aac
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2023-03-15 10:36:56 +0100

    Revert "Add path, method and code to HTTPRequestTimes (#537)"
    This reverts commit 39436b85481fa4f040173c030bde42b03a678f30.

--- loadtest/user/userentity/user.go
+++ loadtest/user/userentity/user.go
@@ -72,7 +72,7 @@ type ueTransport struct {
 func (t *ueTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	startTime := time.Now()
 	resp, err := t.transport.RoundTrip(req)
-	t.ue.observeHTTPRequestTimes(req.URL.Path, req.Method, resp.StatusCode, time.Since(startTime).Seconds())
+	t.ue.observeHTTPRequestTimes(time.Since(startTime).Seconds())
 	if os.IsTimeout(err) {
 		t.ue.incHTTPTimeouts(req.URL.Path, req.Method)
 	}
