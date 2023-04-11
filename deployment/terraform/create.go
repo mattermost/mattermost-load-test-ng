0c61e241b6f9c28591d9a45fee57cd7262ecc3dc
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2023-04-11 13:49:53 +0200

    Update dependencies (#579)
    * Run make update-dependencies
    
    * Run go get github.com/mattermost-server/v6@50aabf6
    
    * Fix import paths

--- deployment/terraform/create.go
+++ deployment/terraform/create.go
@@ -18,9 +18,9 @@ import (
 	"github.com/mattermost/mattermost-load-test-ng/deployment/terraform/assets"
 	"github.com/mattermost/mattermost-load-test-ng/deployment/terraform/ssh"
 
-	"github.com/mattermost/mattermost-server/v6/config"
 	"github.com/mattermost/mattermost-server/v6/model"
-	"github.com/mattermost/mattermost-server/v6/shared/mlog"
+	"github.com/mattermost/mattermost-server/v6/server/config"
+	"github.com/mattermost/mattermost-server/v6/server/platform/shared/mlog"
 )
 
 const cmdExecTimeoutMinutes = 30
