66e03707715f2ad94b84a08c9ff1a51bf20ae527
wiggin77 <wiggin77@warpmail.net>
2023-05-23 18:02:45 -0400

    add InjectAction to cli

--- cmd/ltctl/loadtest.go
+++ cmd/ltctl/loadtest.go
@@ -156,3 +156,49 @@ func RunLoadTestStatusCmdF(cmd *cobra.Command, args []string) error {
 
 	return nil
 }
+
+func RunInjectActionCmdF(cmd *cobra.Command, args []string) error {
+	config, err := getConfig(cmd)
+	if err != nil {
+		return err
+	}
+
+	t, err := terraform.New("", config)
+	if err != nil {
+		return fmt.Errorf("failed to create terraform engine: %w", err)
+	}
+
+	action := args[0]
+
+	status, errInject := t.InjectAction(action)
+	if errInject != nil {
+		fmt.Println("Failed to inject action ", action, ": ", errInject)
+		return errInject
+	}
+
+	tfOutput, err := t.Output()
+	if err != nil {
+		return err
+	}
+
+	prometheusURL := fmt.Sprintf("http://%s:9090", tfOutput.MetricsServer.PublicIP)
+	helper, err := prometheus.NewHelper(prometheusURL)
+	if err != nil {
+		return fmt.Errorf("failed to create prometheus.Helper: %w", err)
+	}
+
+	errInfo, err := getErrorsInfo(helper, status.StartTime)
+	if err != nil {
+		return err
+	}
+
+	usersCount, err := getUsersCount(helper)
+	if err != nil {
+		return err
+	}
+
+	printCoordinatorStatus(status, errInfo, usersCount)
+	fmt.Println("Action ", action, " injected successfully.")
+
+	return nil
+}
