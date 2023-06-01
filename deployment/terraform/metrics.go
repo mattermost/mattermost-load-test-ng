ddfd6eb23d638c12a29a355a30a764aacfff65ad
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2023-06-01 13:14:47 +0200

    Monitor agents in Pyroscope (#595)

--- deployment/terraform/metrics.go
+++ deployment/terraform/metrics.go
@@ -142,6 +142,7 @@ func (t *Terraform) setupMetrics(extAgent *ssh.ExtAgent) error {
 	mlog.Info("Updating Pyroscope config", mlog.String("host", t.output.MetricsServer.PublicIP))
 	pyroscopeConfigFile := fmt.Sprintf(pyroscopeConfig,
 		strings.Join(mmTargets, ","),
+		strings.Join(ltTargets, ","),
 	)
 	rdr = strings.NewReader(pyroscopeConfigFile)
 	if out, err := sshc.Upload(rdr, "/etc/pyroscope/server.yml", true); err != nil {
