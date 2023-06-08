078667999040e450a543a48894cc69b44d9c4755
Agniva De Sarker <agnivade@yahoo.co.in>
2023-06-08 19:43:14 +0530

    Using the correct done function (#603)
    * Using the correct done function
    
    * Adding idleTime of 1s for joinChannel
    
    Without this the GetPostsForChannel call
    was taking too much time

--- loadtest/control/gencontroller/controller.go
+++ loadtest/control/gencontroller/controller.go
@@ -141,7 +141,7 @@ func (c *GenController) Run() {
 			idleTimeMs: 1000,
 		},
 	}
-	c.runActions(actions, func() bool { return st.get("channels") >= c.config.NumChannels })
+	c.runActions(actions, func() bool { return st.get(StateTargetChannels) >= c.config.NumChannels })
 
 	actions = map[string]userAction{
 		"joinTeam": {
@@ -152,7 +152,7 @@ func (c *GenController) Run() {
 		"joinChannel": {
 			run:        c.joinChannel,
 			frequency:  int(math.Ceil(float64(c.config.NumChannels))) * 2, // making this proportional to number of channels.
-			idleTimeMs: 0,
+			idleTimeMs: 1000,
 		},
 		"createPost": {
 			run:        c.createPost,
@@ -187,10 +187,13 @@ func (c *GenController) Run() {
 	}
 
 	c.runActions(actions, func() bool {
-		return st.get("teams") >= c.config.NumTeams &&
-			st.get("channels") >= c.config.NumChannels && // having this again just for clarity
-			st.get("posts") >= c.config.NumPosts &&
-			st.get("reactions") >= c.config.NumReactions
+		return st.get(StateTargetTeams) >= c.config.NumTeams &&
+			st.get(StateTargetChannels) >= c.config.NumChannels && // having this again just for clarity
+			st.get(StateTargetPosts) >= c.config.NumPosts &&
+			st.get(StateTargetReactions) >= c.config.NumReactions &&
+			st.get(StateTargetPostReminders) >= c.config.NumPostReminders &&
+			st.get(StateTargetSidebarCategories) >= c.config.NumSidebarCategories &&
+			st.get(StateTargetFollowedThreads) >= c.config.NumFollowedThreads
 	})
 }
 
