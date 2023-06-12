e535dd073667918cb3f7d26c1823178a918bac43
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2023-06-12 14:16:01 +0200

    Split the channels target into its four types

--- loadtest/control/gencontroller/config.go
+++ loadtest/control/gencontroller/config.go
@@ -5,7 +5,6 @@ package gencontroller
 
 import (
 	"errors"
-	"math"
 
 	"github.com/mattermost/mattermost-load-test-ng/defaults"
 )
@@ -15,8 +14,14 @@ import (
 type Config struct {
 	// The target number of teams to be created.
 	NumTeams int64 `default:"2" validate:"range:[0,]"`
-	// The target number of channels to be created.
-	NumChannels int64 `default:"20" validate:"range:[0,]"`
+	// The target number of direct messages to be created.
+	NumChannelsDM int64 `default:"20" validate:"range:[0,]"`
+	// The target number of group messages to be created.
+	NumChannelsGM int64 `default:"20" validate:"range:[0,]"`
+	// The target number of private channels to be created.
+	NumChannelsPrivate int64 `default:"20" validate:"range:[0,]"`
+	// The target number of public channels to be created.
+	NumChannelsPublic int64 `default:"20" validate:"range:[0,]"`
 	// The target number of posts to be created.
 	NumPosts int64 `default:"1000" validate:"range:[0,]"`
 	// The target number of reactions to be created.
@@ -33,18 +38,6 @@ type Config struct {
 	PercentRepliesInLongThreads float64 `default:"0.05" validate:"range:[0,1]"`
 	// The percentage of post that are marked as urgent.
 	PercentUrgentPosts float64 `default:"0.001" validate:"range:[0,1]"`
-
-	// Percentages of channels to be created, grouped by type.
-	// The total sum of these values must be equal to 1.
-
-	// The percentage of public channels to be created.
-	PercentPublicChannels float64 `default:"0.2" validate:"range:[0,1]"`
-	// The percentage of private channels to be created.
-	PercentPrivateChannels float64 `default:"0.1" validate:"range:[0,1]"`
-	// The percentage of direct channels to be created.
-	PercentDirectChannels float64 `default:"0.6" validate:"range:[0,1]"`
-	// The percentage of group channels to be created.
-	PercentGroupChannels float64 `default:"0.1" validate:"range:[0,1]"`
 
 	// Indicates the distribution of chanel members within channels.
 	ChannelMembersDistribution []ChannelMemberDistribution
@@ -78,11 +71,6 @@ func ReadConfig(configFilePath string) (*Config, error) {
 // IsValid reports whether a given gencontroller.Config is valid or not.
 // Returns an error if the validation fails.
 func (c *Config) IsValid() error {
-	percentChannels := c.PercentPublicChannels + c.PercentPrivateChannels + c.PercentDirectChannels + c.PercentGroupChannels
-	if (math.Round(percentChannels*100) / 100) != 1 {
-		return errors.New("sum of percentages for channels should be equal to 1")
-	}
-
 	totalPercent := 0.0
 	for _, item := range c.ChannelMembersDistribution {
 		totalPercent += item.Probability
@@ -102,4 +90,8 @@ func (c *Config) IsValid() error {
 	}
 
 	return nil
+}
+
+func (c *Config) NumTotalChannels() int64 {
+	return c.NumChannelsDM + c.NumChannelsGM + c.NumChannelsPrivate + c.NumChannelsPublic
 }
