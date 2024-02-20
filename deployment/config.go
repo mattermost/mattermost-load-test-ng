81e57701596dabba5a44a3d26a3b7aae3c64e18e
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2024-02-20 18:56:21 +0100

    Bump version to v1.14.0 (#699)

--- deployment/config.go
+++ deployment/config.go
@@ -71,7 +71,7 @@ type Config struct {
 	// URL from where to download load-test-ng binaries and configuration files.
 	// The configuration files provided in the package will be overridden in
 	// the deployment process.
-	LoadTestDownloadURL   string `default:"https://github.com/mattermost/mattermost-load-test-ng/releases/download/v1.14.0-rc1/mattermost-load-test-ng-v1.14.0-rc1-linux-amd64.tar.gz" validate:"url"`
+	LoadTestDownloadURL   string `default:"https://github.com/mattermost/mattermost-load-test-ng/releases/download/v1.14.0/mattermost-load-test-ng-v1.14.0-linux-amd64.tar.gz" validate:"url"`
 	ElasticSearchSettings ElasticSearchSettings
 	JobServerSettings     JobServerSettings
 	LogSettings           logger.Settings
