6a45c4c9c223ee8a0ecea3181936bf2121c37fbf
Elias Nahum <nahumhbl@gmail.com>
2024-04-25 13:45:44 +0800

    nginx upload set to 250M

--- deployment/terraform/strings.go
+++ deployment/terraform/strings.go
@@ -172,7 +172,7 @@ http {
 `
 
 const nginxProxyCommonConfig = `
-client_max_body_size 50M;
+client_max_body_size 250M;
 proxy_set_header Host $http_host;
 proxy_set_header X-Real-IP $remote_addr;
 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
