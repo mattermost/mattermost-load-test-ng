b06add02859947b8fe0164607c3b37becd0c3eb7
Agniva De Sarker <agnivade@yahoo.co.in>
2024-05-22 10:18:30 +0530

    Check DB status before destroy
    We check if the DB is available or not before destroying.
    Because going ahead with the destroy if the DB is stopped
    messes up with the Terraform state.
    
    https://mattermost.atlassian.net/browse/MM-58236

--- cmd/ltctl/main.go
+++ cmd/ltctl/main.go
@@ -47,6 +47,16 @@ func RunDestroyCmdF(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return fmt.Errorf("failed to create terraform engine: %w", err)
 	}
+
+	status, err := t.DBStatus()
+	if err != nil {
+		return fmt.Errorf("failed to get DB status: %w", err)
+	}
+
+	if status != dbAvailable {
+		return fmt.Errorf("The database isn't available at the moment. Its status is %q. Please check this, and then try again", status)
+	}
+
 	return t.Destroy()
 }
 
