b2d4959add9c9e5bb278c166881afbfc38408508
Felipe Martin <me@fmartingr.com>
2024-07-08 15:27:59 +0200

    validate saml authentication type

--- api/agent.go
+++ api/agent.go
@@ -508,7 +508,7 @@ func getUserCredentials(usersFilePath string, _ *loadtest.Config) ([]user, error
 				return nil, fmt.Errorf("invalid custom authentication found in %q", email)
 			}
 			authService = split[0]
-			if authService != userentity.AuthenticationTypeOpenID && authService != userentity.AuthenticationTypeMattermost {
+			if authService != userentity.AuthenticationTypeOpenID && authService != userentity.AuthenticationTypeSAML && authService != userentity.AuthenticationTypeMattermost {
 				return nil, fmt.Errorf("invalid custom authentication type %q", authService)
 			}
 
