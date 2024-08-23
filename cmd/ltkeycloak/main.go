6f5a0c99d30791161eae1027940b9908ef079841
Felipe Martin <812088+fmartingr@users.noreply.github.com>
2024-08-23 13:58:14 +0200

    ltkeycloak and SAML improvements (#758)
    * basic command
    
    adds the sync from_mattermost command that retrieves all mattermost
    users and creates them in keycloak if they do not exist.
    
    * migrate users from keycloak to mattermost
    
    * retrieve keycloak credentials from config
    
    * missing flag
    
    * changes from feat/disable-proxy-server
    
    * changes from feat/disable-proxy-server
    
    * refactor into functions
    
    * fixed contexts
    
    * non-buffered channels
    
    * Update cmd/ltkeycloak/from_mattermost.go
    
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
    
    * Update deployment/terraform/keycloak.go
    
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
    
    * typo
    
    * better error handling, wait for goros
    
    * avoid locking on errors
    
    * allow customizable siteurl flag
    
    * Added README
    
    * refactor to separate function
    
    * fixed not defering waitgroup call
    
    ---------
    
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>

---
+++ cmd/ltkeycloak/main.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	// How much time to wait for a single operation to complete (all requests used during the
+	// migration of an user)
+	operationTimeout = 30 * time.Second
+)
+
+func MakeSyncCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:               "sync",
+		Short:             "Sync users between from Mattermost to Keycloak",
+		PersistentPostRun: func(_ *cobra.Command, _ []string) { os.Unsetenv("MM_SERVICEENVIRONMENT") },
+	}
+
+	cmd.PersistentFlags().StringP("keycloak-host", "", "http://localhost:8484", "keycloak host")
+	cmd.PersistentFlags().String("mattermost-host", "", "The Mattermost host to migrate users from")
+	cmd.PersistentFlags().BoolP("dry-run", "", false, "perform a dry run without making any changes")
+
+	cmd.AddCommand(MakeSyncFromMattermostCommand())
+	return cmd
+}
+
+func MakeKeylcoakCommand() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:          "ltkeycloak",
+		SilenceUsage: true,
+		Short:        "Utilities to interact with Mattermost and Keycloak deployments",
+	}
+	rootCmd.PersistentFlags().StringP("config", "c", "", "path to the deployer configuration file to use")
+
+	return rootCmd
+}
+
+func main() {
+	rootCmd := MakeKeylcoakCommand()
+	commands := []*cobra.Command{
+		MakeSyncCommand(),
+	}
+	rootCmd.AddCommand(commands...)
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
