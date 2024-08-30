16faf5f9a57a566389c0fe8d360490ae36024d9d
Agniva De Sarker <agnivade@yahoo.co.in>
2024-08-30 11:47:08 +0530

    Restore the saml-idp.crt file in assets
    Without this line, the deployment fails because
    it cannot do t.getAsset

--- deployment/terraform/create.go
+++ deployment/terraform/create.go
@@ -1114,6 +1114,7 @@ func (t *Terraform) init() error {
 	assets.RestoreAssets(t.config.TerraformStateDir, "es_dashboard_data.json")
 	assets.RestoreAssets(t.config.TerraformStateDir, "redis_dashboard_data.json")
 	assets.RestoreAssets(t.config.TerraformStateDir, "keycloak.service")
+	assets.RestoreAssets(t.config.TerraformStateDir, "saml-idp.crt")
 
 	// We lock to make this call safe for concurrent use
 	// since "terraform init" command can write to common files under
