dfd403f7fe1e0f303057994ed7502a9c0fb9af8d
Felipe Martin <812088+fmartingr@users.noreply.github.com>
2024-09-09 08:45:50 +0200

    fix: use proxy_instance_count for the security group (#795)

--- deployment/terraform/assets/cluster.tf
+++ deployment/terraform/assets/cluster.tf
@@ -531,7 +531,7 @@ resource "aws_security_group_rule" "app-to-inbucket" {
 }
 
 resource "aws_security_group" "proxy" {
-  count       = var.app_instance_count > 1 ? 1 : 0
+  count       = var.proxy_instance_count
   name        = "${var.cluster_name}-proxy-security-group"
   description = "Proxy security group for loadtest cluster ${var.cluster_name}"
 
