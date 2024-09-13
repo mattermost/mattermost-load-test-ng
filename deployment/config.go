b7fc39daed727265a45e8eaee514207b13b619e5
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2024-09-13 12:09:18 +0200

    Default proxy count to 1 (#797)
    * Default ProxyInstanceCount to 1
    
    * Add ProxyInstanceCount to deployer samples
    
    * Add ProxyInstanceCount to docs
    
    * Validate proxy configuration

--- deployment/config.go
+++ deployment/config.go
@@ -42,7 +42,7 @@ type Config struct {
 	// Logs the command output (stdout & stderr) to home directory.
 	EnableAgentFullLogs bool `default:"true"`
 	// Number of proxy instances.
-	ProxyInstanceCount int `default:"0" validate:"range:[0,1]"`
+	ProxyInstanceCount int `default:"1" validate:"range:[0,1]"`
 	// Type of the EC2 instance for proxy.
 	ProxyInstanceType string `default:"m4.xlarge" validate:"notempty"`
 	// Path to the SSH public key.
@@ -323,10 +323,21 @@ func (c *Config) IsValid() error {
 		return err
 	}
 
+	if err := c.validateProxyConfig(); err != nil {
+		return err
+	}
+
 	if err := c.validateDBName(); err != nil {
 		return err
 	}
 
+	return nil
+}
+
+func (c *Config) validateProxyConfig() error {
+	if c.AppInstanceCount > 1 && c.ProxyInstanceCount < 1 && c.ServerURL == "" {
+		return fmt.Errorf("the deployment will create more than one app node, but no proxy is being deployed and no external proxy has been configured: either set ProxyInstanceCount to 1, or set ServerURL to the URL of an external proxy")
+	}
 	return nil
 }
 
