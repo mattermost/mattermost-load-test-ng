1979216c08e8f0174b20be75f184c7cd2fa8b001
Felipe Martin <me@fmartingr.com>
2024-09-27 18:35:50 +0200

    added keycloak support for the ssh commands

--- deployment/terraform/utils.go
+++ deployment/terraform/utils.go
@@ -124,6 +124,11 @@ func (t *Terraform) makeCmdForResource(resource string) (*exec.Cmd, error) {
 	}
 
 	// Match against the metrics servers, as well as convenient aliases.
+	if output.KeycloakServer.Tags.Name == resource {
+		return exec.Command("ssh", fmt.Sprintf("ubuntu@%s", output.KeycloakServer.PublicIP)), nil
+	}
+
+	// Match against the proxy or metrics servers, as well as convenient aliases.
 	switch resource {
 	case "metrics", "prometheus", "grafana", output.MetricsServer.Tags.Name:
 		return exec.Command("ssh", fmt.Sprintf("ubuntu@%s", output.MetricsServer.PublicIP)), nil
