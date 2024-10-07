84c6d48c29b9136f9599d591aa552d0e2bd47738
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2024-10-07 17:49:27 +0200

    Remove unneeded, very old code
    This TODO was fixed in
    https://github.com/mattermost/mattermost-load-test-ng/pull/395
    four years ago now.

--- comparison/loadtest.go
+++ comparison/loadtest.go
@@ -62,10 +62,6 @@ func runBoundedLoadTest(t *terraform.Terraform, coordConfig *coordinator.Config,
 
 	mlog.Info("bounded load-test has completed")
 
-	// TODO: remove this once MM-30326 has been merged and a new release
-	// published.
-	status.StopTime = time.Now()
-
 	return status, nil
 }
 
