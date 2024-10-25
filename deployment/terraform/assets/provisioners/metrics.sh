6690466d9516c88ca355f31a00a8fc8f2b93624d
fmartingr <felipe@mattermost.com>
2024-10-25 17:32:04 +0000

    fix prometheus data path

--- deployment/terraform/assets/provisioners/metrics.sh
+++ deployment/terraform/assets/provisioners/metrics.sh
@@ -84,7 +84,7 @@ Group=prometheus
 Type=simple
 ExecStart=/usr/local/bin/prometheus \
 --config.file /etc/prometheus/prometheus.yml \
---storage.tsdb.path /var/lib/prometheus/ \
+--storage.local.path /var/lib/prometheus/ \
 --web.console.templates=/etc/prometheus/consoles \
 --web.console.libraries=/etc/prometheus/console_libraries
 [Install]
