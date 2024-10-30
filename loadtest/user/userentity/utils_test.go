8ea1d23c769476988e90de5a5eecf0f7cba1ea6d
Claudio Costa <cstcld91@gmail.com>
2024-10-30 08:14:58 -0600

    [MM-60297] Fix `loadtest_http_errors_total` metric (#840)
    * Improve nginx logs
    
    * Surface nginx access logs
    
    * Strip IDs from paths used as client metrics labels
    
    * Update test

--- loadtest/user/userentity/utils_test.go
+++ loadtest/user/userentity/utils_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/mattermost/mattermost/server/public/model"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPostsMapToSlice(t *testing.T) {
@@ -23,4 +24,30 @@ func TestPostsMapToSlice(t *testing.T) {
 
 	postsMap = map[string]*model.Post{}
 	assert.Len(t, postsMapToSlice(postsMap), 0)
+}
+
+func TestStripIDs(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name: "empty",
+		},
+		{
+			name:     "no match",
+			input:    "/api/v4/users/me",
+			expected: "/api/v4/users/me",
+		},
+		{
+			name:     "match",
+			input:    "/api/v4/users/w9w9rsucuig4ukzf1tzjzfhy5h/teams/s6kdxh9owfyii8zkq7sianthmh/channels",
+			expected: "/api/v4/users/$ID/teams/$ID/channels",
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			require.Equal(t, tc.expected, stripIDs(tc.input))
+		})
+	}
 }
