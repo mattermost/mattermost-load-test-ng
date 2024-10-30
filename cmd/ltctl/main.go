1b9d04f32850e374caa40cb1046942d046607a4e
Felipe Martin <me@fmartingr.com>
2024-10-30 20:02:28 +0100

    force initData to false

--- cmd/ltctl/main.go
+++ cmd/ltctl/main.go
@@ -28,7 +28,7 @@ func RunCreateCmdF(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("failed to create terraform engine: %w", err)
 	}
 
-	initData := config.DBDumpURI == "" || config.ExternalDBSettings.DataSource != ""
+	initData := false
 	err = t.Create(initData)
 	if err != nil {
 		return fmt.Errorf("failed to create terraform env: %w", err)
