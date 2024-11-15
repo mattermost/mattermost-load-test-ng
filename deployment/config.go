8bec6191696d560ee087272e71f0ff57874e19c5
Kuba <89129941+kczpl@users.noreply.github.com>
2024-11-15 12:02:50 +0100

    Remove comment in config.go
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>

--- deployment/config.go
+++ deployment/config.go
@@ -93,7 +93,6 @@ type Config struct {
 	Report                report.Config
 	// Directory under which the .terraform directory and state files are managed.
 	// It will be created if it does not exist
-	// ./ltstate is the default value used when `deployer.json`` is configured at all
 	TerraformStateDir string `default:"./ltstate" validate:"notempty"`
 	// URI of an S3 bucket whose contents are copied to the bucket created in the deployment
 	S3BucketDumpURI string `default:"" validate:"s3uri"`
