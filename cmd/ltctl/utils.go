6b0b7a875dc9c0037c37f9644b701f31eb38ec0e
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2025-04-25 10:37:18 +0200

    Reword message

--- cmd/ltctl/utils.go
+++ cmd/ltctl/utils.go
@@ -29,7 +29,12 @@ func checkDoNotDestroyMetricsInstanceFlag(cmd *cobra.Command, args []string) err
 	}
 
 	if maintainMetrics {
-		confirmed, err := askForConfirmation("CAUTION! The --do-not-destroy-metrics-instance flag will keep the metrics instance and associated resources alive. This means that you will need to run the destroy command without this flag afterwards to clean up everything. Do you want to continue? [y/n] ")
+		msg := "CAUTION!\n"
+		msg += "The --do-not-destroy-metrics-instance flag will keep the metrics instance and associated resources alive, which means that:\n"
+		msg += "  1. If you recreate your deployment after this command finishes, the metrics instance will be reused instead of creating a new one.\n"
+		msg += "  2. You will need to run `destroy` *without* this flag afterwards to clean everything up.\n"
+		msg += "Do you want to continue? [y/n] "
+		confirmed, err := askForConfirmation(msg)
 		if err != nil {
 			return err
 		}
