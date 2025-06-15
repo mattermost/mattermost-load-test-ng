5ca103af136d15fc9ce01e10cfa586d381f7669d
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2025-06-16 01:04:26 +0530

    added unit test framework

--- browser/playwright.config.ts
+++ browser/playwright.config.ts
@@ -2,14 +2,11 @@ import {defineConfig, devices} from '@playwright/test';
 
 export default defineConfig({
   testDir: './src/tests',
-  outputDir: './test-results',
+  outputDir: './e2etest-results',
   fullyParallel: true,
-  retries: 0,
-  workers: 1, // TODO: Adjust this configuration
   use: {
     trace: 'off',
   },
-
   projects: [
     {
       name: 'chromium',
---
+++ browser/vitest.config.ts
@@ -0,0 +1,9 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['src/**/*.test.ts'],
+    exclude: ['src/**/*.spec.ts'],
+  },
+})
