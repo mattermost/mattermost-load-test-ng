c4ea6f3da18b41d9fb6800a185f1ecb1d6dea88f
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2025-06-18 16:44:01 +0200

    Use the maximum permitted size for RX ring buffer

--- deployment/terraform/create_utils.go
+++ deployment/terraform/create_utils.go
@@ -2,6 +2,7 @@ package terraform
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/mattermost/mattermost-load-test-ng/deployment/terraform/ssh"
@@ -34,4 +35,98 @@ func (t *Terraform) setupPrometheusNodeExporter(sshClient *ssh.Client) error {
 	}
 
 	return nil
+}
+
+// modifyRXSize modifies the size of the receiving buffer of the network card
+// to the maximum permitted by the hardware.
+func modifyRXSize(sshc *ssh.Client) error {
+	rxSizes, err := getEthtoolRxSizes(sshc)
+	if err != nil {
+		return fmt.Errorf("error getting the RX sizes from the ethtool: %w", err)
+	}
+
+	oldRxSize := rxSizes.actualRX
+
+	// Modify RX queue size to the maximum permitted
+	incRxSizeCmd := fmt.Sprintf("sudo ethtool -G $(ip route show to default | awk '{print $5}') rx %d", rxSizes.maxRX)
+	cmd := fmt.Sprintf("%s && sudo sysctl -p && sudo systemctl restart nginx", incRxSizeCmd)
+	if out, err := sshc.RunCommand(cmd); err != nil {
+		return fmt.Errorf("error running ssh command; output: %q; cmd: %q; err: %w", string(out), cmd, err)
+	}
+
+	// Log the actual RX queue size after the modification
+	newRxSizes, err := getEthtoolRxSizes(sshc)
+	if err != nil {
+		return fmt.Errorf("error getting the RX sizes from the ethtool: %w", err)
+	}
+
+	newRxSize := newRxSizes.actualRX
+	if err != nil {
+		return fmt.Errorf("error retrieving actual RX queue size: %w", err)
+	}
+
+	mlog.Info("size of the receiving ring buffer in the proxy has been modified", mlog.Int("old_size", oldRxSize), mlog.Int("new_size", newRxSize), mlog.Int("max_size", newRxSizes.maxRX))
+	return nil
+}
+
+type ethtoolOutput struct {
+	maxRX    int
+	actualRX int
+}
+
+// getEthtoolRxSizes runs the ethtool command and parses its output.
+func getEthtoolRxSizes(sshc *ssh.Client) (ethtoolOutput, error) {
+	getRXCmd := "sudo ethtool -g ens3"
+	out, err := sshc.RunCommand(getRXCmd)
+	if err != nil {
+		return ethtoolOutput{}, fmt.Errorf("error running ssh command; output: %q; cmd: %q, err: %w", string(out), getRXCmd, err)
+	}
+
+	return parseEthtoolOutputRXSizes(string(out))
+}
+
+// parseEthtoolOutputRXSizes parses the RX ring buffer sizes (maximum permitted
+// and actual) from the output of ethtool -g, which looks like:
+//
+//	Ring parameters for ens3:
+//	Pre-set maximums:
+//	RX:             4096
+//	RX Mini:        n/a
+//	RX Jumbo:       n/a
+//	TX:             4096
+//	Current hardware settings:
+//	RX:             1024
+//	RX Mini:        n/a
+//	RX Jumbo:       n/a
+//	TX:             1024
+func parseEthtoolOutputRXSizes(out string) (ethtoolOutput, error) {
+	lines := strings.Split(out, "\n")
+	rxLines := []string{}
+	prefix := "RX:"
+	for _, line := range lines {
+		if strings.HasPrefix(line, prefix) {
+			noPrefix := strings.TrimPrefix(line, prefix)
+			trimmed := strings.ReplaceAll(noPrefix, "\t", "")
+			rxLines = append(rxLines, trimmed)
+		}
+	}
+
+	if len(rxLines) != 2 {
+		return ethtoolOutput{}, fmt.Errorf("unexpected number of matching RX lines: %d", len(rxLines))
+	}
+
+	rxValues := make([]int, 2)
+	for i, line := range rxLines {
+		val, err := strconv.Atoi(strings.TrimSpace(string(line)))
+		if err != nil {
+			return ethtoolOutput{}, fmt.Errorf("unable to parse integer in line %d: %q; err: %w", i, string(line), err)
+		}
+
+		rxValues[i] = val
+	}
+
+	return ethtoolOutput{
+		maxRX:    rxValues[0],
+		actualRX: rxValues[1],
+	}, nil
 }
