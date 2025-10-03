25061fd89e7a7f8012960b55b8437bf00b8b1565
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2025-10-03 17:40:55 +0200

    Use latest minor version for Postgres v14

--- deployment/terraform/assets/variables.tf
+++ deployment/terraform/assets/variables.tf
@@ -124,7 +124,7 @@ variable "db_engine_version" {
   type = map(any)
   default = {
     "aurora-mysql"      = "8.0.mysql_aurora.3.05.2"
-    "aurora-postgresql" = "14.9"
+    "aurora-postgresql" = "14.18"
   }
 }
 
