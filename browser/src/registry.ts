c4e778884f0ca63f2825a8bfb8431a8e965c5350
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2026-01-20 00:14:10 +0530

    Update simulation ID and descriptions for Mattermost's post and scroll scenario

--- browser/src/registry.ts
+++ browser/src/registry.ts
@@ -7,11 +7,12 @@ import {postAndScrollScenario} from './simulations/post_and_scroll_scenario.js';
 
 export const SimulationsRegistry: SimulationRegistryItem[] = [
   {
-    id: 'postAndScroll',
-    name: 'Post and Scroll scenario',
-    description: 'A basic scenario that posts and scrolls in a channel',
+    id: 'mattermostPostAndScroll',
+    name: 'Mattermost\'s post and scroll scenario',
+    description: 'A basic scenario that posts and scrolls in the Mattermost channels',
     scenario: postAndScrollScenario,
   },
+
   // Here goes the plugins simulations registry
   // after it is imported from the plugin's loadtest-browser package
   // ...PluginsSimulationsRegistry,
