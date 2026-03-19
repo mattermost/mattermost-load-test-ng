c9951f52a9d697f4d282f3f10ab32ee35e5eede2
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2026-03-20 03:15:51 +0530

    Update default logging settings in BrowserLogSettings to enable console logging and set console log level to debug

--- loadtest/control/browsercontroller/config.go
+++ loadtest/control/browsercontroller/config.go
@@ -10,8 +10,8 @@ import (
 // BrowserLogSettings holds information to be used to initialize the logger for the LTBrowser API
 // refer to /browser/src/utils/log.ts
 type BrowserLogSettings struct {
-	EnableConsole bool   `default:"false"`
-	ConsoleLevel  string `default:"error" validate:"oneof:{trace, debug, info, warn, error, fatal}"`
+	EnableConsole bool   `default:"true"`
+	ConsoleLevel  string `default:"debug" validate:"oneof:{trace, debug, info, warn, error, fatal}"`
 	EnableFile    bool   `default:"true"`
 	FileLevel     string `default:"debug" validate:"oneof:{trace, debug, info, warn, error, fatal}"`
 	FileLocation  string `default:"browseragent.log"`
@@ -21,12 +21,16 @@ type BrowserLogSettings struct {
 type Config struct {
 	// The ID of the simulation to run.
 	SimulationId string `default:"mattermostPostAndScroll" validate:"notempty"`
+
 	// Whether to run the browser in headless mode.
 	RunInHeadless bool `default:"true"`
+
 	// The timeout in milliseconds for browser simulations.
 	SimulationTimeoutMs int `default:"60000" validate:"range:[0,]"`
+
 	// Whether to enable plugins in the browser simulation.
 	EnabledPlugins bool `default:"false"`
+
 	// Log settings for the LTBrowser API
 	LogSettings BrowserLogSettings
 }
